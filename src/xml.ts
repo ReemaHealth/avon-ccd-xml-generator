68e79686edebf789feecc16b8bc4229646992e00
Erik Thayer <erik@thayer.io>
2026-06-18 14:34:00 -0500

    HL7 code audit

--- src/xml.ts
+++ src/xml.ts
@@ -1,5 +1,19 @@
 import { Note, NoteAnswer, Patient } from "./types";
 
+const CODE_SYSTEM = {
+  LOINC: { oid: "2.16.840.1.113883.6.1", name: "LOINC" },
+  SNOMED_CT: { oid: "2.16.840.1.113883.6.96", name: "SNOMED CT" },
+  ICD10_CM: { oid: "2.16.840.1.113883.6.90", name: "ICD10CM" },
+  HL7_ACT_CLASS: { oid: "2.16.840.1.113883.5.6", name: "HL7ActClass" },
+  HL7_ACT_STATUS: { oid: "2.16.840.1.113883.5.14", name: "ActStatus" },
+  HL7_ADMIN_GENDER: { oid: "2.16.840.1.113883.5.1", name: "AdministrativeGender" },
+  HL7_CONFIDENTIALITY: { oid: "2.16.840.1.113883.5.25", name: "Confidentiality" },
+  NUCC: { oid: "2.16.840.1.113883.6.101", name: "NUCC" },
+} as const;
+
+const statusCode = (code: string): string =>
+  `<statusCode code="${code}" codeSystem="${CODE_SYSTEM.HL7_ACT_STATUS.oid}" codeSystemName="${CODE_SYSTEM.HL7_ACT_STATUS.name}"/>`;
+
 const escapeText = (value: string): string =>
   value
     .replace(/&/g, "&amp;")
@@ -100,10 +114,10 @@ export function buildCcdXml(note: Note, patient: Patient): string {
   <templateId root="2.16.840.1.113883.10.20.22.1.2" extension="2015-08-01"/>
   <templateId root="2.16.840.1.113883.10.20.22.1.2"/>
   <id extension="${escapeAttr(noteIdExt)}" root="2.16.840.1.113883.19.5.99999.1"/>
-  <code code="34133-9" displayName="Summarization of Episode Note" codeSystem="2.16.840.1.113883.6.1" codeSystemName="LOINC"/>
+  <code code="34133-9" displayName="Summarization of Episode Note" codeSystem="${CODE_SYSTEM.LOINC.oid}" codeSystemName="${CODE_SYSTEM.LOINC.name}"/>
   <title>CCD for consultation ${escapeText(noteIdExt)}</title>
   <effectiveTime value="${escapeAttr(createdDate)}"/>
-  <confidentialityCode code="N" displayName="normal" codeSystem="2.16.840.1.113883.5.25" codeSystemName="Confidentiality"/>
+  <confidentialityCode code="N" displayName="normal" codeSystem="${CODE_SYSTEM.HL7_CONFIDENTIALITY.oid}" codeSystemName="${CODE_SYSTEM.HL7_CONFIDENTIALITY.name}"/>
   <languageCode code="en-US"/>
   <setId extension="${escapeAttr(noteIdExt)}" root="2.16.840.1.113883.19.5.99999.19"/>
   <versionNumber value="1"/>
@@ -115,7 +129,7 @@ ${phone ? `      <telecom value="tel:${escapeAttr(phone)}" use="MC"/>\n` : ""}  
           <given>${escapeText(givenName)}</given>
           <family>${escapeText(familyName)}</family>
         </name>
-        <administrativeGenderCode code="${gender.code}" displayName="${gender.display}" codeSystem="2.16.840.1.113883.5.1" codeSystemName="AdministrativeGender"/>
+        <administrativeGenderCode code="${gender.code}" displayName="${gender.display}" codeSystem="${CODE_SYSTEM.HL7_ADMIN_GENDER.oid}" codeSystemName="${CODE_SYSTEM.HL7_ADMIN_GENDER.name}"/>
         <birthTime value="${escapeAttr(birthDate)}"/>
       </patient>
     </patientRole>
@@ -140,7 +154,7 @@ ${phone ? `      <telecom value="tel:${escapeAttr(phone)}" use="MC"/>\n` : ""}  
       <performer typeCode="PRF">
         <assignedEntity>
           <id root="2.16.840.1.113883.4.6"/>
-          <code code="${escapeAttr(snomedCode ?? "")}" codeSystem="2.16.840.1.113883.6.96" codeSystemName="SNOMED-CT" displayName="Community Guide"/>
+          <code code="${escapeAttr(snomedCode ?? "")}" codeSystem="${CODE_SYSTEM.SNOMED_CT.oid}" codeSystemName="${CODE_SYSTEM.SNOMED_CT.name}" displayName="Community Guide"/>
         </assignedEntity>
       </performer>
     </serviceEvent>
@@ -150,24 +164,19 @@ ${phone ? `      <telecom value="tel:${escapeAttr(phone)}" use="MC"/>\n` : ""}  
       <component>
         <section>
           <templateId root="2.16.840.1.113883.10.20.22.2.22.1" extension="2015-08-01"/>
-          <code code="46240-8" codeSystem="2.16.840.1.113883.6.1" codeSystemName="LOINC"/>
+          <code code="46240-8" codeSystem="${CODE_SYSTEM.LOINC.oid}" codeSystemName="${CODE_SYSTEM.LOINC.name}" displayName="History of encounters"/>
           <title>ENCOUNTERS</title>
           <text>
-            <table border="1" width="100%">
-              <thead>
-                <tr><th>Evaluation</th></tr>
-              </thead>
-              <tbody>
-                <tr><td ID="Encounter1">${escapeText(visitDisplay)}</td></tr>
-              </tbody>
-            </table>
+            <paragraph>
+              <content ID="Encounter1">${escapeText(visitDisplay)}</content>
+            </paragraph>
           </text>
           <entry typeCode="DRIV">
             <encounter classCode="ENC" moodCode="EVN">
               <templateId root="2.16.840.1.113883.10.20.22.4.49" extension="2015-08-01"/>
               <templateId root="2.16.840.1.113883.10.20.22.4.49"/>
               <id root="${escapeAttr(noteIdExt)}"/>
-              <code code="${escapeAttr(snomedCode ?? "")}" codeSystemName="SNOMED-CT" codeSystem="2.16.840.1.113883.6.96">
+              <code code="${escapeAttr(snomedCode ?? "")}" codeSystem="${CODE_SYSTEM.SNOMED_CT.oid}" codeSystemName="${CODE_SYSTEM.SNOMED_CT.name}" displayName="${escapeAttr(visitDisplay)}">
                 <originalText>
                   <reference value="#Encounter1"/>
                 </originalText>
@@ -177,13 +186,13 @@ ${phone ? `      <telecom value="tel:${escapeAttr(phone)}" use="MC"/>\n` : ""}  
               <performer>
                 <assignedEntity>
                   <id root="2.16.840.1.113883.4.6"/>
-                  <code code="172V00000X" codeSystem="2.16.840.1.113883.6.101" codeSystemName="NUCC Health Care Provider Taxonomy" displayName="Community Guide"/>
+                  <code code="172V00000X" codeSystem="${CODE_SYSTEM.NUCC.oid}" codeSystemName="${CODE_SYSTEM.NUCC.name}" displayName="Community Guide"/>
                 </assignedEntity>
               </performer>
               <participant typeCode="PART">
                 <participantRole classCode="ASSIGNED">
                   <id root="2.16.840.1.113883.4.6"/>
-                  <code code="${escapeAttr(snomedCode ?? "")}" codeSystem="2.16.840.1.113883.6.96" codeSystemName="SNOMED-CT" displayName="${escapeAttr(visitDisplay)}"/>
+                  <code code="${escapeAttr(snomedCode ?? "")}" codeSystem="${CODE_SYSTEM.SNOMED_CT.oid}" codeSystemName="${CODE_SYSTEM.SNOMED_CT.name}" displayName="${escapeAttr(visitDisplay)}"/>
                 </participantRole>
               </participant>
             </encounter>
@@ -201,7 +210,7 @@ function buildProblemValue(
   refId: string,
 ): string {
   if (code) {
-    return `<value xsi:type="CD" code="${escapeAttr(code)}" codeSystem="2.16.840.1.113883.6.90" codeSystemName="ICD-10-CM">
+    return `<value xsi:type="CD" code="${escapeAttr(code)}" codeSystem="${CODE_SYSTEM.ICD10_CM.oid}" codeSystemName="${CODE_SYSTEM.ICD10_CM.name}">
                     <originalText>
                       <reference value="#${escapeAttr(refId)}"/>
                     </originalText>
@@ -228,11 +237,11 @@ function buildProblemsSection(
         }))
       : [{ code: null as string | null, refId: "Problem1" }];
 
-  const rows = items
+  const narrative = items
     .map(({ code, refId }) =>
       code
-        ? `                <tr><td>${escapeText(code)}</td><td ID="${escapeAttr(refId)}">${escapeText(description)}</td></tr>`
-        : `                <tr><td></td><td ID="${escapeAttr(refId)}">${escapeText(description)}</td></tr>`,
+        ? `            <paragraph>${escapeText(code)}: <content ID="${escapeAttr(refId)}">${escapeText(description)}</content></paragraph>`
+        : `            <paragraph><content ID="${escapeAttr(refId)}">${escapeText(description)}</content></paragraph>`,
     )
     .join("\n");
 
@@ -241,16 +250,16 @@ function buildProblemsSection(
       ({ code, refId }) => `          <entry typeCode="DRIV">
             <act classCode="ACT" moodCode="EVN">
               <templateId root="2.16.840.1.113883.10.20.22.4.3" extension="2015-08-01"/>
-              <code code="CONC" codeSystem="2.16.840.1.113883.5.6"/>
-              <statusCode code="active"/>
+              <code code="CONC" codeSystem="${CODE_SYSTEM.HL7_ACT_CLASS.oid}" codeSystemName="${CODE_SYSTEM.HL7_ACT_CLASS.name}"/>
+              ${statusCode("active")}
               <effectiveTime>
                 <low value="${escapeAttr(effectiveDate)}"/>
               </effectiveTime>
               <entryRelationship typeCode="SUBJ">
                 <observation classCode="OBS" moodCode="EVN">
                   <templateId root="2.16.840.1.113883.10.20.22.4.4" extension="2015-08-01"/>
-                  <code code="55607006" codeSystem="2.16.840.1.113883.6.96" codeSystemName="SNOMED CT" displayName="Problem"/>
-                  <statusCode code="completed"/>
+                  <code code="55607006" codeSystem="${CODE_SYSTEM.SNOMED_CT.oid}" codeSystemName="${CODE_SYSTEM.SNOMED_CT.name}" displayName="Problem"/>
+                  ${statusCode("completed")}
                   <effectiveTime>
                     <low value="${escapeAttr(effectiveDate)}"/>
                   </effectiveTime>
@@ -265,17 +274,10 @@ function buildProblemsSection(
   return `      <component>
         <section>
           <templateId root="2.16.840.1.113883.10.20.22.2.5.1" extension="2015-08-01"/>
-          <code code="11450-4" codeSystem="2.16.840.1.113883.6.1" codeSystemName="LOINC" displayName="Problem List"/>
+          <code code="11450-4" codeSystem="${CODE_SYSTEM.LOINC.oid}" codeSystemName="${CODE_SYSTEM.LOINC.name}" displayName="Problem List"/>
           <title>PROBLEMS</title>
           <text>
-            <table border="1" width="100%">
-              <thead>
-                <tr><th>Code</th><th>Description</th></tr>
-              </thead>
-              <tbody>
-${rows}
-              </tbody>
-            </table>
+${narrative}
           </text>
 ${entries}
         </section>
